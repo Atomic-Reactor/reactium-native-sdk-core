ab636e102ce1712b0739a36fbc24505ad04d1586
Cam Tullos <>
2021-09-18 09:44:20 -1000

    rename Actinium sub modules directory.

--- src/actinium/index.js
+++ src/actinium/index.js
@@ -1,23 +1,5 @@
-import pkg from '~/package.json';
-import { io } from 'socket.io-client';
-import Actinium from './lib';
+import Actinium from './sdk';
 
-class SDK {
-    constructor() {
-        Object.entries(Actinium).forEach(([key, value]) => (this[key] = value));
+export * from './sdk';
 
-        const { serverURL } = pkg.actinium;
-        if (typeof serverURL === 'string') {
-            const ioURL = serverURL.replace(/^http/i, 'ws');
-            this.IO = io(ioURL.replace('/api', ''), {
-                path: '/actinium.io',
-                autoConnect: false,
-                transports: ['polling'],
-            });
-        }
-    }
-
-    init() {}
-}
-
-export default new SDK();
+export default Actinium;
